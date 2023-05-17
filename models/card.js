8d0154ae66b5986c91f18179d43bfbf4da84f7ee
jllcf <jorge@itonn.com.br>
2023-05-17 20:16:46 -0300

    Funcionalidade de login e criacao de card

---
+++ models/card.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const db = require('./db');
+
+const card = db.define('card', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+});
+
+//usuarios.sync();
+
+module.exports = usuarios;
