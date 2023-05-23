55e948cb6ccb936237ac1a611c238c480012e93a
gcmarcello <marcellogcoelho@outlook.com>
2023-05-23 18:20:33 -0300

    refactor: new login controller

--- src/api/controllers/loginController.js
+++ src/api/controllers/loginController.js
@@ -1,43 +1,13 @@
-const { object, string } = require("yup");
-const Login = require("../models/login");
-//const { aAdmin } = require("../middlewares/auth");
+const { loginService } = require("../services/loginService");
 
 const login_check = async (req, res) => {
-  const schema = object({
-    user_email: string().email("Email é obrigatório."),
-    user_password: string().min(
-      6,
-      "Senha precisa conter 6 ou mais caracteres."
-    ),
-  });
+  const login = await loginService(req.body);
 
-  try {
-    const validation = await schema.validate(req.body, { abortEarly: false });
-  } catch (error) {
-    return res.status(400).json({
-      erro: true,
-      mensagem: error.errors,
-    });
+  if (!login) {
+    return res.status(401).json({ message: "Email ou senha incorretos.", type: "error" });
   }
 
-  const {user_email, user_password} = req.body;
-
-  try {
-    const user = await User.findOne({ where: {user_email, user_password}});
-    if (!user) {
-      return res
-      .status(404)
-      .json({ erro: true, mensagem: "Usuário não encontrado"});
-    }
-      return res.status(200).json({ message: "Login efetuado com sucesso!"});
-    
-  }catch(error) {console.error(error); return res
-  .status(500)
-  .json({erro: true, mensagem:"Erro ao realizar o login"});
-}
+  return res.status(200).json({ message: "Login efetuado.", type: "success", token: login });
 };
 
-
-
 module.exports = { login_check };
-
